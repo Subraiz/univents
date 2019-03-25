29257f1fecaac4c6d3e5dd0481b559acc508a826
Subraiz <subraizahmed@gmail.com>
2019-03-25 18:57:00 -0400

    iOS Fixes

--- screens/PrivacyModal.js
+++ screens/PrivacyModal.js
@@ -8,9 +8,10 @@ import {
   Dimensions,
   SafeAreaView,
   Button,
-  Alert
+  Alert,
+  ScrollView
 } from "react-native";
-import Terms from "../consants/Terms";
+import Terms from "../constants/Terms";
 
 const screenWidth = Dimensions.get("window").width;
 const screenHeight = Dimensions.get("window").height;
@@ -27,7 +28,44 @@ class PrivacyModal extends Component {
         transparent={true}
       >
         <SafeAreaView style={styles.container}>
-          <View style={styles.mainContent} />
+          <View style={styles.mainContent}>
+            <Text
+              style={{
+                alignSelf: "center",
+                fontSize: 20,
+                fontWeight: "600",
+                marginTop: 15
+              }}
+            >
+              Privacy & Terms
+            </Text>
+            <View
+              style={{
+                width: "100%",
+                height: 2,
+                backgroundColor: "#E7E7E7",
+                marginTop: 8
+              }}
+            />
+            <View style={{ height: "85%" }}>
+              <ScrollView
+                style={{
+                  padding: 20
+                }}
+              >
+                <Text>{terms}</Text>
+              </ScrollView>
+            </View>
+            <View
+              style={{
+                width: "100%",
+                height: 2,
+                backgroundColor: "#E7E7E7",
+                marginTop: 8
+              }}
+            />
+            <Button title="Got It" onPress={this.props.onClose} />
+          </View>
         </SafeAreaView>
       </Modal>
     );
@@ -39,10 +77,11 @@ const styles = {
     flex: 1,
     height: screenHeight,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    backgroundColor: "rgba(0,0,0,.3)"
   },
   mainContent: {
-    height: screenHeight * 0.75,
+    height: screenHeight * 0.85,
     width: screenWidth * 0.85,
     backgroundColor: "white",
     borderRadius: 35
