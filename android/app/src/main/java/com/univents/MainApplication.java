3715582a9d60c48de45e4b94b64fd50e9a669966
Subraiz <subraizahmed@gmail.com>
2019-03-28 06:51:04 -0400

    Finally FIxed Android
    Styaed up until 7AM doing this wow

--- android/app/src/main/java/com/univents/MainApplication.java
+++ android/app/src/main/java/com/univents/MainApplication.java
@@ -2,11 +2,13 @@ package com.univents;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.facebook.react.ReactApplication;
 import com.horcrux.svg.SvgPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
+import io.fabric.sdk.android.Fabric;
 import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -23,42 +25,43 @@ import com.airbnb.android.react.lottie.LottiePackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
+private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+                return BuildConfig.DEBUG;
+        }
 
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new ImagePickerPackage(),
-            new VectorIconsPackage(),
-            new MapsPackage(),
-            new RNCameraPackage(),
-            new RNFetchBlobPackage(),
-            new SvgPackage(),
-            new RNFSPackage(),
-            new FastImageViewPackage(),
-            new LottiePackage()
-      );
-    }
+        @Override
+        protected List<ReactPackage> getPackages() {
+                return Arrays.<ReactPackage>asList(
+                        new MainReactPackage(),
+                        new ImagePickerPackage(),
+                        new VectorIconsPackage(),
+                        new MapsPackage(),
+                        new RNCameraPackage(),
+                        new RNFetchBlobPackage(),
+                        new SvgPackage(),
+                        new RNFSPackage(),
+                        new FastImageViewPackage(),
+                        new LottiePackage()
+                        );
+        }
 
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
+        @Override
+        protected String getJSMainModuleName() {
+                return "index";
+        }
+};
 
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
+@Override
+public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
+}
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
+@Override
+public void onCreate() {
+        super.onCreate();
+        Fabric.with(this, new Crashlytics());
+        SoLoader.init(this, /* native exopackage */ false);
+}
 }
